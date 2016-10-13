acd9a95cd42839712edbb6422ed9687f3bc21a0e
bokyeong1015 <bokyeong1015@gmail.com>
2016-10-13 18:16:05 +0900

    remove

--- lib1_AlignFace_NormalizeInput/pipeline_modules_functions/INface_toolbox/postprocessors/Contents.m
+++
@@ -1,5 +0,0 @@
-% POSTPROCESSORS
-%
-% Files
-%   histtruncate         - The function truncates the ends of an image histogram.
-%   robust_postprocessor - The function performs postprocessing of the photometrically normalized image X
